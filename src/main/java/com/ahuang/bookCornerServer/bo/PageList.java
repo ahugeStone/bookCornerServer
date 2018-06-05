4737b621dca3ecd1018cf4cdf9093dd55acb8c09
Shi Huang <742969055@qq.com>
2018-06-05 22:34:34 +0800

    优化sql，优化page

--- src/main/java/com/ahuang/bookCornerServer/bo/PageList.java
+++ src/main/java/com/ahuang/bookCornerServer/bo/PageList.java
@@ -44,14 +44,16 @@ public class PageList<T> {
 		if(null == totalNum) {
 			totalNum = list.size();
 		}
-		Integer totalPageNum = totalNum%pageSize == 0? totalNum/pageSize:totalNum/pageSize+1;
-		Integer endNum = startNum + pageSize;
-		endNum  = endNum > totalNum? totalNum:endNum;
-		startNum = startNum > endNum? endNum:startNum;
-		Integer currentPageNum = endNum%pageSize == 0? startNum/pageSize:startNum/pageSize+1;
-		currentPageNum = currentPageNum==0? 1:currentPageNum;
-		currentPageNum = currentPageNum > totalPageNum? totalPageNum:currentPageNum;
-		boolean isLastPage = endNum == totalNum;
+		Integer totalPageNum = totalNum%pageSize == 0? totalNum/pageSize:totalNum/pageSize+1;// 总页数
+		Integer endNum = startNum + pageSize; 
+		endNum  = endNum > totalNum? totalNum:endNum; // 当前页最后元素id不能大于总数
+		startNum = startNum > endNum? endNum:startNum; // 当前页第一项不能大于最后一项
+		Integer currentPageNum = (startNum+1)%pageSize == 0? startNum/pageSize:startNum/pageSize+1;//计算当前页
+		currentPageNum = currentPageNum==0? 1:currentPageNum; //当前页不能为0
+		currentPageNum = currentPageNum > totalPageNum? totalPageNum:currentPageNum;//当前页不能大于总页数
+		boolean isLastPage = endNum == totalNum; //是否为最后一页
+		currentPageNum = isLastPage? totalPageNum:currentPageNum;//如果是最后一页，设定当前页为最后一页
+		pageSize = list.size();//设置当前页面大小
 		
 		this.startNum = startNum;
 		this.totalPageNum = totalPageNum;
--- src/main/java/com/ahuang/bookCornerServer/mapper/BookBaseInfoMapper.java
+++ src/main/java/com/ahuang/bookCornerServer/mapper/BookBaseInfoMapper.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.SelectProvider;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -22,6 +21,18 @@ import com.ahuang.bookCornerServer.entity.BookBaseInfoEntity;
 public interface BookBaseInfoMapper extends PagingAndSortingRepository<BookBaseInfoEntity,String>{
 	String TABLE_NAEM = " BOOK_BASEINFO ";
 	String SELECT_FIELDS = " bookId, bookName, bookWriter, bookBrief, bookType, bookStatus, bookSource, bookBuyer, bookTime, bookRemark, bookLikeNum, bookCommentNum, recTime ";
+	String BOOK_LIST_WHERE = "<where>"
+			+ "<if test='bookName!=null and bookName!=\"\"'>"
+			+ "<bind name=\"pattern\" value=\"'%' + bookName + '%'\" />" 
+			+ "and bookName like #{pattern} "
+			+ "</if>"
+			+ "<if test='bookType!=null and bookType!=\"\"'>"
+			+ "and bookType = #{bookType}"
+			+ "</if>"
+			+ "<if test='bookStatus!=null and bookStatus!=\"\"'>"
+			+ "and bookStatus = #{bookStatus}"
+			+ "</if>"
+		+"</where>";
 	
 	@Select("Select " + SELECT_FIELDS + " from " + TABLE_NAEM + " where bookId=#{id}")
 	public BookBaseInfoEntity queryById(Integer id);
@@ -34,18 +45,7 @@ public interface BookBaseInfoMapper extends PagingAndSortingRepository<BookBaseI
 	
 	@Select( "<script>"
 	+  "select " + SELECT_FIELDS + " from  " + TABLE_NAEM 
-	+ "<where>"
-		+ "<if test='bookName!=null and bookName!=\"\"'>"
-		+ "<bind name=\"pattern\" value=\"'%' + bookName + '%'\" />" 
-		+ "and bookName like #{pattern} "
-		+ "</if>"
-		+ "<if test='bookType!=null and bookType!=\"\"'>"
-		+ "and bookType = #{bookType}"
-		+ "</if>"
-		+ "<if test='bookStatus!=null and bookStatus!=\"\"'>"
-		+ "and bookStatus = #{bookStatus}"
-		+ "</if>"
-	+"</where>"
+	+ BOOK_LIST_WHERE
 	+ "<if test='num!=null and pageSize != null '>"
 	+ "  limit #{num}, #{pageSize}"
 	+ "</if>"  
@@ -54,53 +54,8 @@ public interface BookBaseInfoMapper extends PagingAndSortingRepository<BookBaseI
 	
 	@Select("<script>"
 			+ "select count(1) from " + TABLE_NAEM
-			+ "<where>"
-			+ "<if test='bookName!=null and bookName!=\"\"'>"
-			+ "<bind name=\"pattern\" value=\"'%' + bookName + '%'\" />" 
-			+ "and bookName like #{pattern} "
-			+ "</if>"
-			+ "<if test='bookType!=null and bookType!=\"\"'>"
-			+ "and bookType = #{bookType}"
-			+ "</if>"
-			+ "<if test='bookStatus!=null and bookStatus!=\"\"'>"
-			+ "and bookStatus = #{bookStatus}"
-			+ "</if>"
-		+"</where>"
+			+ BOOK_LIST_WHERE
 		+ "</script>")
 	public Integer queryBookInfoNum(Map<String, Object> param);
 	
-	class BookProvider {  
-		/**
-		 * 
-		* @Title: queryBookListPage
-		* @Description: 获取select语句，可以是获取列表也可以是获取总数
-		* @param @param params
-		* @param @return    
-		* @return String    返回类型
-		* @author ahuang  
-		* @date 2018年6月5日 下午9:52:23
-		* @version V1.0
-		* @throws
-		 */
-        public String queryBookListPage(Map<String, Object> params) { 
-        	String selectName = (boolean)params.get("isCount")? "count(1)":SELECT_FIELDS;
-            String sql = "<script>"
-        			+ "select " + params.get("isCount") + " from " + TABLE_NAEM
-        			+ "<where>"
-        			+ "<if test='bookName!=null and bookName!=\"\"'>"
-        			+ "<bind name=\"pattern\" value=\"'%' + bookName + '%'\" />" 
-        			+ "and bookName like #{pattern} "
-        			+ "</if>"
-        			+ "<if test='bookType!=null and bookType!=\"\"'>"
-        			+ "and bookType = #{bookType}"
-        			+ "</if>"
-        			+ "<if test='bookStatus!=null and bookStatus!=\"\"'>"
-        			+ "and bookStatus = #{bookStatus}"
-        			+ "</if>"
-        		+"</where>"
-        		+ "</script>";  
-
-            return sql;  
-        }  
-    } 
 }
