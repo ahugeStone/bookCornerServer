c18f5e34f8c6d0a91b1984a96dc1db4b638a1d84
Shi Huang <742969055@qq.com>
2018-07-31 22:26:30 +0800

    修改表中语法错误，删除无用字段isThumbup，这个字段干啥的？

--- docs/book（结构和数据）添加字段 添加两张新表.sql
+++ docs/book（结构和数据）添加字段 添加两张新表.sql
@@ -21,61 +21,61 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for book_baseinfo
 -- ----------------------------
 
-alter table `book_baseinfo` add `bookScore` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '得分:默认为0';
+alter table `BOOK_BASEINFO` add `bookScore` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '得分:默认为0';
 
 -- ----------------------------
 -- Table structure for cust_users
 -- ----------------------------
 
-alter table `cust_users` add `userEmail` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '员工邮箱:默认为0';
+alter table `CUST_USERS` add `userEmail` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '员工邮箱';
 
 
 -- ----------------------------
 -- Table structure for book_commentrecord
 -- ----------------------------
 
-alter table `book_commentrecord` add `commentLikeNum` int(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 0 COMMENT '评论点赞数';
-alter table `book_commentrecord` add `isThumbup` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '是否点赞了: 0：否；1：是。默认为0';
+alter table `BOOK_COMMENTRECORD` add COLUMN `commentLikeNum` int(11) NOT NULL DEFAULT 0 COMMENT '评论点赞数';
+-- alter table `BOOK_COMMENTRECORD` add COLUMN `isThumbup` char(1) NOT NULL DEFAULT '0' COMMENT '是否点赞了: 0：否；1：是。默认为0';
 
 -- ----------------------------
 -- Table structure for comment_likerecord
 -- ----------------------------
-DROP TABLE IF EXISTS `comment_likerecord`;
-CREATE TABLE `comment_likerecord`  (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `COMMENT_LIKERECORD`;
+CREATE TABLE `COMMENT_LIKERECORD`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `commentId` int(11) NOT NULL,
-  `openid` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `openid` varchar(100) NOT NULL,
   `recTime` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
   `bookId` int(11) DEFAULT NULL,
-  `headImgUrl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `userName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `headImgUrl` varchar(255) DEFAULT NULL,
+  `userName` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+);
 
 -- ----------------------------
 -- Records of comment_likerecord
 -- ----------------------------
-INSERT INTO `comment_likerecord` VALUES (1, 1, 'oe0Ej0d3pqW_Bi5Bmg2lvpSdOtRE', '2018-07-25 16:42:56', 1, '12121212', '黄实');
+INSERT INTO `COMMENT_LIKERECORD` VALUES (1, 1, 'oe0Ej0d3pqW_Bi5Bmg2lvpSdOtRE', '2018-07-25 16:42:56', 1, '12121212', '黄实');
 
 
 -- ----------------------------
 -- Table structure for message_baseinfo
 -- ----------------------------
-DROP TABLE IF EXISTS `message_baseinfo`;
-CREATE TABLE `message_baseinfo`  (
-  `id` int(11) NOT NULL,
-  `operationType` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `operationContent` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+DROP TABLE IF EXISTS `MESSAGE_BASEINFO`;
+CREATE TABLE `MESSAGE_BASEINFO`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `operationType` char(1) NOT NULL,
+  `operationContent` varchar(500) DEFAULT NULL,
   `operationTime` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
-  `userName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `userName` varchar(20) NOT NULL,
   `bookId` int(11) NOT NULL,
-  `bookName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `bookName` varchar(100) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+);
 
 -- ----------------------------
 -- Records of message_baseinfo
 -- ----------------------------
-INSERT INTO `message_baseinfo` VALUES (1, '1', '1', '2018-07-25 11:38:23', '黄实', 1, '中国震撼');
+INSERT INTO `MESSAGE_BASEINFO` VALUES (1, '1', '1', '2018-07-25 11:38:23', '黄实', 1, '中国震撼');
 
 SET FOREIGN_KEY_CHECKS = 1;
